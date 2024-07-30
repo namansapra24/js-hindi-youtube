fe00bcd4ce234dd59b0480e0a13ea8007ec446f7
namansapra24 <namansinghsapra@gmail.com>
2024-07-30 09:21:08 +0000

    saving

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountEmail = "Naman@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState;
+
+// accountId = "3" // not allowed
+
+/*
+Prefer  not to use var in js
+*/
+
+accountEmail = "Naman@g.com"
+accountPassword = "123123"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use Strict"
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 3) // do not amek it hard to read like this
+
+console.log("Naman");
+
+let name = "Naman"
+let age = 18
+let isLoggedIn = false
+
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>yet to be defined
+//symbol => unique
+
+//object
+
+console.log(typeof "Naman");
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
